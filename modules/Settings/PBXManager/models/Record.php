c84055bae42c55893176488dba6b8098bcc70994
Jaime <jayepesviloria@gmail.com>
2021-07-28 09:32:06 -0500

    fix save settings

--- modules/Settings/PBXManager/models/Record.php
+++ modules/Settings/PBXManager/models/Record.php
@@ -69,7 +69,7 @@ class Settings_PBXManager_Record_Model extends Settings_Vtiger_Record_Model {
     
     public function save() {
 		$db = PearDatabase::getInstance();
-		$parameters = '';
+		$parameters = array();
 		$selectedGateway = $this->get('gateway');
                 $connector = new PBXManager_PBXManager_Connector;
                 
